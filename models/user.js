713c7c89e2bdc55a3c49aeecb481cfb35ff27318
Andrii Savchuk <andriksr@gmail.com>
2017-07-10 18:29:26 +0200

    added Register Route functionality

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+//User Schema
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserByID = function(id, callback) {
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(id, callback) {
+  const query = {username: username};
+  User.findOne(query, callback);
+}
