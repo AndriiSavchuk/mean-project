bc184c6cce4ca2c49021a305c1238022967bc533
Andrii Savchuk <andriksr@gmail.com>
2017-07-10 18:03:15 +0200

    fixed users rout in users.js

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 //Register route
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
   res.send('REGISTER');
 });
 
 //Authenticate route
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
   res.send('AUTHENTICATE');
 });
 
 //Profile route
 router.get('/profile', (req, res, next) => {
   res.send('PROFILE');
-});
-
-//Validate route
-router.get('/validate', (req, res, next) => {
-  res.send('VALIDATE');
 });
 
 module.exports = router;
