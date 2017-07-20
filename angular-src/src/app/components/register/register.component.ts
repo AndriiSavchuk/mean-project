fafc21422af5f953483c4ffc40340eb46467c73f
Andrii Savchuk <andriksr@gmail.com>
2017-07-20 18:55:35 +0200

    added flashMessages plugin

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
 
+export class RegisterComponent implements OnInit {
     name: String;
     username: String;
     email: String;
     password: String;
 
-  constructor(private validateService: ValidateService) { }
+  constructor(private validateService: ValidateService, private flashMessagesSerice: FlashMessagesService) { }
 
   ngOnInit() {
   }
@@ -28,13 +29,13 @@ export class RegisterComponent implements OnInit {
 
     // Required Fields
     if (!this.validateService.validateRegister(user)) {
-      console.log('Please fill in all fields');
+      this.flashMessagesSerice.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
       return false;
     }
 
     // VAlodate Email
     if (!this.validateService.validateRegister(user.email)) {
-      console.log('Please use a valid email');
+      this.flashMessagesSerice.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
       return false;
     }
   }
