b739a287b00525a7d917c3368c54689cf420f0c8
Andrii Savchuk <andriksr@gmail.com>
2017-08-01 17:18:55 +0200

    added profile ngOninit functionality

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -33,9 +33,16 @@ export class AuthService {
 
   getProfile() {
     let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
     return this.http.get('http://localhost:3000/users/profile', { headers: headers })
       .map(res => res.json());
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
   }
 
   logout() {
